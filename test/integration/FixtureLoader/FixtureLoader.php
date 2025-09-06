425f438602b242c5c7bc29b9642121e72145e9f1
Julien Guittard <julien.guittard@me.com>
2025-09-06 23:01:11 +0200

    fix: missing return annotations on PhpDbIntegrationTest\Adapter\Mysql\FixtureLoader
    Signed-off-by: Julien Guittard <julien.guittard@me.com>

--- test/integration/FixtureLoader/FixtureLoader.php
+++ test/integration/FixtureLoader/FixtureLoader.php
@@ -7,7 +7,13 @@ namespace PhpDbIntegrationTest\Adapter\Mysql\FixtureLoader;
 // phpcs:ignore WebimpressCodingStandard.NamingConventions.Interface.Suffix
 interface FixtureLoader
 {
+    /**
+     * @return void
+     */
     public function createDatabase();
 
+    /**
+     * @return void
+     */
     public function dropDatabase();
 }
