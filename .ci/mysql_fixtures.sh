74bf3a2a77d87aa44c42be6715dc7c72181cae53
Joey Smith <jsmith@webinertia.net>
2025-09-15 03:35:15 -0500

    revert changes as a test Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- .ci/mysql_fixtures.sh
+++ .ci/mysql_fixtures.sh
@@ -2,4 +2,4 @@
 
 echo "Configure MySQL test database"
 
-mysql --user=gha --password=password -e "create database phpdb_test;"
+mysql --user=root --password=Password123 -e "create database laminasdb_test;"
